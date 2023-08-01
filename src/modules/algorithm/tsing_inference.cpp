9688787a86afa87652e304d0fb9103c710343f09
zhdotcai <zhehongcai@gmail.com>
2023-08-01 08:30:46 +0000

    🐞 fix: 移除注释

--- src/modules/algorithm/tsing_inference.cpp
+++ src/modules/algorithm/tsing_inference.cpp
@@ -20,39 +20,39 @@ struct TsingInference::Impl {
 };
 
 TsingInference::TsingInference() : impl_(std::make_unique<TsingInference::Impl>()) {
-    // impl_->surf_params = std::make_unique<BufSurfaceParams>();
-    // impl_->surf_ptr = std::make_shared<gddeploy::BufSurfaceWrapper>(new BufSurface, false);
-    // impl_->surf_ptr->GetBufSurface()->surface_list = impl_->surf_params.get();
+    impl_->surf_params = std::make_unique<BufSurfaceParams>();
+    impl_->surf_ptr = std::make_shared<gddeploy::BufSurfaceWrapper>(new BufSurface, false);
+    impl_->surf_ptr->GetBufSurface()->surface_list = impl_->surf_params.get();
 }
 
 TsingInference::~TsingInference() {
-    // if (impl_->alg_impl) { impl_->alg_impl->WaitTaskDone(); }
+    if (impl_->alg_impl) { impl_->alg_impl->WaitTaskDone(); }
 }
 
 bool TsingInference::init(const ModParms &parms) {
-    // impl_->alg_impl = std::make_unique<gddeploy::InferAPI>();
-    // impl_->alg_impl->Init("", parms.mod_path, "", gddeploy::ENUM_API_TYPE::ENUM_API_SESSION_API);
+    impl_->alg_impl = std::make_unique<gddeploy::InferAPI>();
+    impl_->alg_impl->Init("", parms.mod_path, "", gddeploy::ENUM_API_TYPE::ENUM_API_SESSION_API);
 
-    // auto model_type = impl_->alg_impl->GetModelType();
-    // if (model_type == "detection") {
-    //     impl_->algo_type = AlgoType::kDetection;
-    // } else if (model_type == "classification") {
-    //     impl_->algo_type = AlgoType::kClassification;
-    // } else if (model_type == "pose") {
-    //     impl_->algo_type = AlgoType::kPose;
-    // } else if (model_type == "segmentation") {
-    //     impl_->algo_type = AlgoType::kSegmentation;
-    // } else if (model_type == "action") {
-    //     impl_->algo_type = AlgoType::kAction;
-    // } else if (model_type == "ocr_det") {
-    //     impl_->algo_type = AlgoType::kOCR_DET;
-    // } else if (model_type == "ocr_rec") {
-    //     impl_->algo_type = AlgoType::kOCR_REC;
-    // } else {
-    //     throw std::runtime_error("Undefined model type.");
-    // }
+    auto model_type = impl_->alg_impl->GetModelType();
+    if (model_type == "detection") {
+        impl_->algo_type = AlgoType::kDetection;
+    } else if (model_type == "classification") {
+        impl_->algo_type = AlgoType::kClassification;
+    } else if (model_type == "pose") {
+        impl_->algo_type = AlgoType::kPose;
+    } else if (model_type == "segmentation") {
+        impl_->algo_type = AlgoType::kSegmentation;
+    } else if (model_type == "action") {
+        impl_->algo_type = AlgoType::kAction;
+    } else if (model_type == "ocr_det") {
+        impl_->algo_type = AlgoType::kOCR_DET;
+    } else if (model_type == "ocr_rec") {
+        impl_->algo_type = AlgoType::kOCR_REC;
+    } else {
+        throw std::runtime_error("Undefined model type.");
+    }
 
-    // AbstractAlgo::init(parms, AlgoType::kDetection, impl_->alg_impl->GetLabels());
+    AbstractAlgo::init(parms, AlgoType::kDetection, impl_->alg_impl->GetLabels());
 
     return true;
 }
@@ -60,39 +60,39 @@ bool TsingInference::init(const ModParms &parms) {
 void TsingInference::inference(const std::string &task_name, const std::shared_ptr<nodes::FrameInfo> &info,
                                const InferType type) {
 
-    // impl_->surf_ptr->GetBufSurface()->surface_list[0].width = info->src_frame->data->width;
-    // impl_->surf_ptr->GetBufSurface()->surface_list[0].height = info->src_frame->data->height;
-    // impl_->surf_ptr->GetBufSurface()->surface_list[0].color_format = GDDEPLOY_BUF_COLOR_FORMAT_NV12;
-    // impl_->surf_ptr->GetBufSurface()->surface_list[0].pitch = 1;
-    // impl_->surf_ptr->GetBufSurface()->surface_list[0].data_ptr = info->src_frame->data->buf[0]->data;
+    impl_->surf_ptr->GetBufSurface()->surface_list[0].width = info->src_frame->data->width;
+    impl_->surf_ptr->GetBufSurface()->surface_list[0].height = info->src_frame->data->height;
+    impl_->surf_ptr->GetBufSurface()->surface_list[0].color_format = GDDEPLOY_BUF_COLOR_FORMAT_NV12;
+    impl_->surf_ptr->GetBufSurface()->surface_list[0].pitch = 1;
+    impl_->surf_ptr->GetBufSurface()->surface_list[0].data_ptr = info->src_frame->data->buf[0]->data;
 
-    // gddeploy::PackagePtr in = gddeploy::Package::Create(1);
-    // in->data[0]->Set(impl_->surf_ptr);
-    // in->data[0]->SetUserData(info->infer_frame_idx);
+    gddeploy::PackagePtr in = gddeploy::Package::Create(1);
+    in->data[0]->Set(impl_->surf_ptr);
+    in->data[0]->SetUserData(info->infer_frame_idx);
 
-    // impl_->alg_impl->InferAsync(
-    //     in, [this](gddeploy::Status status, gddeploy::PackagePtr data, gddeploy::any user_data) {
-    //         std::vector<algo::AlgoOutput> vec_output;
-    //         if (data->data[0]->HasMetaValue()) {
-    //             auto result = data->data[0]->GetMetaData<gddeploy::InferResult>();
-    //             for (auto result_type : result.result_type) {
-    //                 if (result_type == gddeploy::GDD_RESULT_TYPE_DETECT) {
-    //                     for (uint32_t i = 0; i < result.detect_result.detect_imgs.size(); i++) {
-    //                         for (auto &obj : result.detect_result.detect_imgs[i].detect_objs) {
-    //                             vec_output.emplace_back(
-    //                                 AlgoOutput{.class_id = obj.class_id,
-    //                                            .prob = obj.score,
-    //                                            .box = {obj.bbox.x, obj.bbox.y, obj.bbox.w, obj.bbox.h}});
-    //                         }
-    //                     }
-    //                 }
-    //             }
+    impl_->alg_impl->InferAsync(
+        in, [this](gddeploy::Status status, gddeploy::PackagePtr data, gddeploy::any user_data) {
+            std::vector<algo::AlgoOutput> vec_output;
+            if (data->data[0]->HasMetaValue()) {
+                auto result = data->data[0]->GetMetaData<gddeploy::InferResult>();
+                for (auto result_type : result.result_type) {
+                    if (result_type == gddeploy::GDD_RESULT_TYPE_DETECT) {
+                        for (uint32_t i = 0; i < result.detect_result.detect_imgs.size(); i++) {
+                            for (auto &obj : result.detect_result.detect_imgs[i].detect_objs) {
+                                vec_output.emplace_back(
+                                    AlgoOutput{.class_id = obj.class_id,
+                                               .prob = obj.score,
+                                               .box = {obj.bbox.x, obj.bbox.y, obj.bbox.w, obj.bbox.h}});
+                            }
+                        }
+                    }
+                }
 
-    //             if (infer_callback_) {
-    //                 infer_callback_(data->data[0]->GetUserData<int64_t>(), impl_->algo_type, vec_output);
-    //             }
-    //         }
-    //     });
+                if (infer_callback_) {
+                    infer_callback_(data->data[0]->GetUserData<int64_t>(), impl_->algo_type, vec_output);
+                }
+            }
+        });
 
     infer_callback_(info->infer_frame_idx, impl_->algo_type, {});
 }
