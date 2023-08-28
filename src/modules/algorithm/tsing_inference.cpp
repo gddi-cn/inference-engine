98eff54c4b593540f2890b54abcea0132e6abeae
zhdotcai <zhehongcai@gmail.com>
2023-08-28 06:42:58 +0000

    🎈 perf: 减低编码质量

--- src/modules/algorithm/tsing_inference.cpp
+++ src/modules/algorithm/tsing_inference.cpp
@@ -52,6 +52,89 @@ bool TsingInference::init(const ModParms &parms) {
 
 void TsingInference::inference(const std::string &task_name, const std::shared_ptr<nodes::FrameInfo> &info,
                                const InferType type) {
+    // int img_width = info->src_frame->data->width;
+    // int img_height = info->src_frame->data->height;
+    // int img_format = info->src_frame->data->format;
+    // auto img_linesize = info->src_frame->data->linesize;
+    // auto img_data = info->src_frame->data->data;
+
+    // BufSurfaceCreateParams params;
+    // params.mem_type = GDDEPLOY_BUF_MEM_TS;
+    // params.device_id = 0;
+    // params.width = img_width;
+    // params.height = img_height;
+    // params.color_format = GDDEPLOY_BUF_COLOR_FORMAT_NV12;
+    // params.force_align_1 = 1;
+    // params.bytes_per_pix = 1;
+    // params.size = 0;
+    // params.batch_size = 1;
+
+    /************** 复现情况一：申请内存并拷贝（推理使用拷贝帧数据）一路25帧卡顿（640x640） **************/
+    // auto surf_ptr = std::make_shared<gddeploy::BufSurfaceWrapper>(new BufSurface, true);
+    // BufSurface *surf = surf_ptr->GetBufSurface();
+    // memset(surf, 0, sizeof(BufSurface));
+    // gddeploy::CreateSurface(&params, surf);
+    // auto pstFrameInfo = (VIDEO_FRAME_INFO_S *)surf_ptr->GetBufSurface()->surface_list[0].data_ptr;
+    // auto size = av_image_get_buffer_size((AVPixelFormat)img_format, img_width, img_height, 32);
+    // av_image_copy_to_buffer((uint8_t *)pstFrameInfo->stVFrame.u64VirAddr[0], size, img_data,
+    //                         (const int *)info->src_frame->data->linesize, (AVPixelFormat)img_format, img_width,
+    //                         img_height, 1);
+    // gddeploy::PackagePtr in = gddeploy::Package::Create(1);
+    // in->data[0]->Set(surf_ptr);
+    // in->data[0]->SetUserData(info->infer_frame_idx);
+    /*******************************************************************************************/
+
+    /************* 复现情况二：复用内存空间并拷贝（推理使用拷贝帧数据）一路25帧流畅（640x640）*************/
+    // static std::shared_ptr<gddeploy::BufSurfaceWrapper> surf_ptr;
+    // if (!surf_ptr) {
+    //     surf_ptr = std::make_shared<gddeploy::BufSurfaceWrapper>(new BufSurface, true);
+    //     BufSurface *surf = surf_ptr->GetBufSurface();
+    //     memset(surf, 0, sizeof(BufSurface));
+    //     gddeploy::CreateSurface(&params, surf);
+    // }
+    // auto pstFrameInfo = (VIDEO_FRAME_INFO_S *)surf_ptr->GetBufSurface()->surface_list[0].data_ptr;
+    // auto size = av_image_get_buffer_size((AVPixelFormat)img_format, img_width, img_height, 32);
+    // av_image_copy_to_buffer((uint8_t *)pstFrameInfo->stVFrame.u64VirAddr[0], size, img_data,
+    //                         (const int *)info->src_frame->data->linesize, (AVPixelFormat)img_format, img_width,
+    //                         img_height, 1);
+    // gddeploy::PackagePtr in = gddeploy::Package::Create(1);
+    // in->data[0]->Set(surf_ptr);
+    // in->data[0]->SetUserData(info->infer_frame_idx);
+    /*******************************************************************************************/
+
+    /************* 复现情况四：复用内存空间并拷贝（推理使用原始帧数据）一路25帧异常（640x640）*************/
+    // static std::shared_ptr<gddeploy::BufSurfaceWrapper> tmp_surf_ptr;
+    // if (!tmp_surf_ptr) {
+    //     tmp_surf_ptr = std::make_shared<gddeploy::BufSurfaceWrapper>(new BufSurface, true);
+    //     BufSurface *surf = tmp_surf_ptr->GetBufSurface();
+    //     memset(surf, 0, sizeof(BufSurface));
+    //     gddeploy::CreateSurface(&params, surf);
+    // }
+    // auto pstFrameInfo = (VIDEO_FRAME_INFO_S *)tmp_surf_ptr->GetBufSurface()->surface_list[0].data_ptr;
+    // auto size = av_image_get_buffer_size((AVPixelFormat)img_format, img_width, img_height, 32);
+    // av_image_copy_to_buffer((uint8_t *)pstFrameInfo->stVFrame.u64VirAddr[0], size, img_data,
+    //                         (const int *)info->src_frame->data->linesize, (AVPixelFormat)img_format, img_width,
+    //                         img_height, 1);
+
+    // auto buf_surf = std::shared_ptr<gddeploy::BufSurfaceWrapper>(new gddeploy::BufSurfaceWrapper(new BufSurface, false),
+    //                                                              [](gddeploy::BufSurfaceWrapper *ptr) {
+    //                                                                  delete ptr->GetBufSurface()->surface_list;
+    //                                                                  delete ptr->GetBufSurface();
+    //                                                                  delete ptr;
+    //                                                              });
+    // buf_surf->GetBufSurface()->surface_list = new BufSurfaceParams;
+    // buf_surf->GetBufSurface()->surface_list[0].width = info->src_frame->data->width;
+    // buf_surf->GetBufSurface()->surface_list[0].height = info->src_frame->data->height;
+    // buf_surf->GetBufSurface()->surface_list[0].color_format = GDDEPLOY_BUF_COLOR_FORMAT_NV12;
+    // buf_surf->GetBufSurface()->surface_list[0].pitch = 1;
+    // buf_surf->GetBufSurface()->surface_list[0].data_ptr = info->src_frame->data->buf[0]->data;
+
+    // gddeploy::PackagePtr in = gddeploy::Package::Create(1);
+    // in->data[0]->Set(buf_surf);
+    // in->data[0]->SetUserData(info->infer_frame_idx);
+    /*******************************************************************************************/
+
+    // 正常情况：不拷贝帧数据（推理使用原始帧数据）一路25帧异常
     auto buf_surf = std::shared_ptr<gddeploy::BufSurfaceWrapper>(new gddeploy::BufSurfaceWrapper(new BufSurface, false),
                                                                  [](gddeploy::BufSurfaceWrapper *ptr) {
                                                                      delete ptr->GetBufSurface()->surface_list;
@@ -92,6 +175,8 @@ void TsingInference::inference(const std::string &task_name, const std::shared_p
                 }
             }
         });
+
+    // infer_callback_(info->infer_frame_idx, impl_->algo_type, {});
 }
 
 AlgoType TsingInference::inference(const std::string &task_name, const std::shared_ptr<nodes::FrameInfo> &info,
